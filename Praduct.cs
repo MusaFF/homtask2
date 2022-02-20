a180f86532e422c9087f57ac839bb13234c5cd5b
MusaFF <99997526+MusaFF@users.noreply.github.com>
2022-02-20 20:02:41 +0400

    Add files via upload

---
+++ Praduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task2
+{
+    class Praduct
+    {
+        public string Name;
+        public string Pirce;
+        public string volume;
+        public string count;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Praduct praduct = new Praduct();
+            praduct.Name = "Coca-cola";
+            praduct.Pirce = "1.80";
+            praduct.volume = "2L";
+            praduct.count = "15";
+            //Coca=Cola mehsulu
+
+            Console.WriteLine($"Mehsulun adi: {praduct.Name}\nQiymet: {praduct.Pirce}\nTutum Hecim: {praduct.volume}\nSayi: {praduct.count}\n");
+            {
+                Praduct praduc = new Praduct();
+                praduct.Name = "Bizon";
+                praduct.Pirce = "1";
+                praduct.volume = "350ml";
+                praduct.count = "20";
+                //Bizon Mehsulu
+
+                Console.WriteLine($"Mehsulun adi: {praduct.Name}\nQiymet: {praduct.Pirce}\nTutum Hecim: {praduct.volume}\nSayi: {praduct.count}\n");
+            }
+            {
+                Praduct pradduct = new Praduct();
+                praduct.Name = "RedBUll";
+                praduct.Pirce = "3.50";
+                praduct.volume = "450ml";
+                praduct.count = "10";
+                //Coca=Cola mehsulu
+
+                Console.WriteLine($"Mehsulun adi: {praduct.Name}\nQiymet: {praduct.Pirce}\nTutum Hecim: {praduct.volume}\nSayi: {praduct.count}\n");
+            }
+            {
+                Praduct prraduct = new Praduct();
+                praduct.Name = "Milk";
+                praduct.Pirce = "1.50";
+                praduct.volume = "2L";
+                praduct.count = "8";
+                //Coca=Cola mehsulu
+
+                Console.WriteLine($"Mehsulun adi: {praduct.Name}\nQiymet: {praduct.Pirce}\nTutum Hecim: {praduct.volume}\nSayi: {praduct.count}\n");
+            }
+        }
+    }
+}
